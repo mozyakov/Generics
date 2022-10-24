9150bc1471e31e7c96ebc320f51882aac33449fb
mozyakov <s.mozyakov@gmail.com>
2022-10-24 08:47:40 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        ArrayList list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        Random random1 = new Random();
+        int randomInt = random1.nextInt(5);
+
+
     }
 }
