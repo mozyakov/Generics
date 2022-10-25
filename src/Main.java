aa4d0e2b659d46974107650e4ef506d345e62966
mozyakov <s.mozyakov@gmail.com>
2022-10-25 15:58:44 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,11 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        ArrayList list = new ArrayList<>();
-        list.add(0);
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
+
         Random random1 = new Random();
         int randomInt = random1.nextInt(5);
 
-        System.out.println(list);
-        System.out.println("==================");
-        /*LinkedList list2 = new LinkedList<>();
-        for (Object o : list) {
-            list2.add(o);
-            list2 = new LinkedList<>(list);
-        }
-        System.out.println(list2);
-         */
 
-        //List<Integer> list1 = new ArrayList<>();
-        LinkedList list2 = new LinkedList<>();
-        for (int i=0; i < list.size(); i++) {
-            list2.add(i);
-            list2 = new LinkedList<>(list);
-        }
-
-        System.out.println(list2);
 
 
 
