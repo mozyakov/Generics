0effad82a593b363980f31ab2e5aefaabd2a3d45
mozyakov <s.mozyakov@gmail.com>
2022-10-27 16:52:37 +0700

    конструктор работает, метод getItem (вызов Arrays.toString)

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,14 +1,16 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class MagicBox<T> {
     protected T[] item;
     //private int count = 0; //это показатель что коробка пустая, с null T value не получался boolean add
-
+    //protected T[] item = new T[3];
+    //public Account[] accounts = new Account[3];
     public MagicBox(int maxCount) {
         item = (T[]) new Object[maxCount];
-
+        //T[] item = (T[]) new Object[maxCount];
+        //this.accounts = new Account[3];
     }
-
     /*public MagicBox(int maxCount) { //конструктор
         items = MagicBox[] savedArray  = new MagicBox[3] ;
     }*/
@@ -17,29 +19,30 @@ public class MagicBox<T> {
         T[] arrayItems = (T[]) new Object[3];
     }*/
 
-    public void putValue(T[] item) { //параметр метода типа T
+    /*public void putValue(T[] item) { //параметр метода типа T
         //int i[] = ;
         this.item = item ;
-    }
+    }*/
 
-    public boolean add(T item) {
-        if (item == null) {  //значит бокс пустой
-            this.item = (T[]) item;
-            //macCount += 1;  //для запрета дальнейших попыток положить в бокс
-            return true;
-        } else {
-            return false;
-        }
+    public boolean add(T value) { //void add(Account account)
+        for (int i = 0; i < item.length; i++) { //for (int i = 0; i < accounts.length; i++) {
+            if (item == null) {  //значит бокс пустой  if (accounts[i] == null) {
+                item[i] =  value ;  //accounts[i] = account;
+                //macCount += 1;  //для запрета дальнейших попыток положить в бокс
+                return true;
+            }
+        }return false;
     }
-
-    public void showBox() { //показ элементов ящика
+    /*public void showBox() { //показ элементов ящика
         for(T i : item ) {
-            System.out.print(i + " ");
+            System.out.println(i + " ");
         }
+    }*/
+    public void getItem() { // возвращает объект типа T
+            System.out.println(Arrays.toString(item));
+        //return item;
     }
-    public T[] getItem() { // возвращает объект типа T
-        return item;
-    }
+}
 
     /*public <T> T pick() throws RuntimeException {
         for(int i =0; i < item.length; i++) {
@@ -51,6 +54,5 @@ public class MagicBox<T> {
         int randomInt = random.nextInt()(item.length);
         return randomInt;
     }*/
-    //public Account[] accounts = new Account[3];
-}
+//public Account[] accounts = new Account[3];
 
--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,17 @@ public class Main {
         //int randomInt = random1.nextInt(5);
 
         MagicBox<String> boxStr = new MagicBox<>(3);
-        boxStr.showBox();
+        boxStr.getItem();
         //boxStr.putValue([0], "First");
         boxStr.add("Fe");
+        System.out.println();
+        boxStr.getItem();
         System.out.println();
         System.out.println();
 
         MagicBox<Integer> boxInt = new MagicBox<>(5);
-        System.out.println();
-        boxInt.showBox();
+        //System.out.println();
+        boxInt.getItem();
 
 
 
