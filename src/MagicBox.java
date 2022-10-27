35ff95aa0ea5f17a98d9999cc5a9a0d78a50cb6e
mozyakov <s.mozyakov@gmail.com>
2022-10-27 19:15:49 +0700

    Задание завершено.

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,53 +1,31 @@
-//import java.util.Arrays;
-//import java.util.Random;
 import java.util.*;
-
 public class MagicBox<T> {
     protected T[] item;
-    //protected T[] item = new T[3];
     public MagicBox(int maxCount) {
         item = (T[]) new Object[maxCount];
-        //T[] item = (T[]) new Object[maxCount];
-        //this.accounts = new Account[3];
     }
-    /*public MagicBox(int maxCount) { //конструктор
-        items = MagicBox[] savedArray  = new MagicBox[3] ;
-    }*/
-
-    /*public MagicBox() {
-        T[] arrayItems = (T[]) new Object[3];
-    }*/
-
-    /*public void putValue(T[] item) { //параметр метода типа T
-        //int i[] = ;
-        this.item = item ;
-    }*/
-    public boolean add(T value) { //void add(Account account)
-        for (int i = 0; i < item.length; i++) { //for (int i = 0; i < accounts.length; i++) {
-            if (item[i] == null) {  //значит бокс пустой  if (accounts[i] == null) {
-                item[i] =  value ;  //accounts[i] = account;
-                //macCount += 1;  //для запрета дальнейших попыток положить в бокс
+    public boolean add(T value) {
+        for (int i = 0; i < item.length; i++) {
+            if (item[i] == null) {
+                item[i] =  value ;
                 return true;
             }
         }return false;
     }
-
-    public void getItem() { // возвращает объект типа T
+    public void getItem() {
             System.out.println(Arrays.toString(item));
-        //return item;
     }
-    public <T> int pick() throws RuntimeException {
+    public T pick() throws RuntimeException {
         for(int i =0; i < item.length; i++) {
             if(item[i] == null) {
-                throw new RuntimeException("ящик не полон, нужно заполнить " + (item.length-i) + " ячеек");
+                throw new RuntimeException("ящик не полон, нужно заполнить " + (item.length - i) + " ячеек");
             }
         }
         Random random = new Random();
         int randomInt = random.nextInt((item.length));
-        return  randomInt;
+        System.out.println(item[(randomInt)]);
+        return  item[(randomInt)];
     }
 }
 
-
-//public Account[] accounts = new Account[3];
 
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,16 @@
-import java.util.*;
 public class Main {
     public static void main(String[] args) {
         MagicBox<String> boxStr = new MagicBox<>(3); //максимум три элемента
-        boxStr.add("Первый");
-        boxStr.getItem();
-
+        boxStr.add("Первый"); //добавляем в ячейку
+        //boxStr.pick(); //если раскомментировать то будет исключение с правильным числом незаполненных ячеек 2
+        boxStr.getItem(); //покажет массив с заполненной первой ячейкой, а остальные null
         boxStr.add("Второй");
-
         boxStr.add("Третий");
         boxStr.add("Четвёртый"); //этот уже не добавится
         boxStr.getItem();
-        boxStr.pick();
+        boxStr.pick(); //покажет значение случайного элемента
 
         System.out.println();
-
 
         MagicBox<Integer> boxInt = new MagicBox<>(3); ////максимум три элемента
         boxInt.add(100);
@@ -21,11 +18,6 @@ public class Main {
         boxInt.add(300);
         boxInt.add(400); //этот уже не добавится
         boxInt.getItem();
-
-
-
-
-
-
+        boxInt.pick(); //покажет значение случайного элемента
     }
 }
