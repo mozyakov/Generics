9d2a9dd360da36c536c68e0b044a2818419e0559
mozyakov <s.mozyakov@gmail.com>
2022-10-27 15:46:35 +0700

    конструктор MagicBox работает

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,9 +1,12 @@
+import java.util.Random;
+
 public class MagicBox<T> {
     protected T[] item;
     //private int count = 0; //это показатель что коробка пустая, с null T value не получался boolean add
 
     public MagicBox(int maxCount) {
         item = (T[]) new Object[maxCount];
+
     }
 
     /*public MagicBox(int maxCount) { //конструктор
@@ -15,18 +18,20 @@ public class MagicBox<T> {
     }*/
 
     public void putValue(T[] item) { //параметр метода типа T
-        this.item = item;
+        //int i[] = ;
+        this.item = item ;
     }
 
-    /*public boolean add(T items) {
-        if (T[] items == null) {  //значит бокс пустой
-            this.item = item;
-            count += 1;  //для запрета дальнейших попыток положить в бокс
+    public boolean add(T item) {
+        if (item == null) {  //значит бокс пустой
+            this.item = (T[]) item;
+            //macCount += 1;  //для запрета дальнейших попыток положить в бокс
             return true;
         } else {
             return false;
         }
-    }*/
+    }
+
     public void showBox() { //показ элементов ящика
         for(T i : item ) {
             System.out.print(i + " ");
@@ -36,7 +41,16 @@ public class MagicBox<T> {
         return item;
     }
 
-
+    /*public <T> T pick() throws RuntimeException {
+        for(int i =0; i < item.length; i++) {
+            if(item[i] == null) {
+                throw new RuntimeException("ящик не полон, можно заполнить " + item.length + " ячеек");
+            }
+        }
+        Random random = new Random();
+        int randomInt = random.nextInt()(item.length);
+        return randomInt;
+    }*/
     //public Account[] accounts = new Account[3];
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        Random random1 = new Random();
-        int randomInt = random1.nextInt(5);
-        String str1 = "First";
-        String str2 = "Second";
-        String str3 = "First";
+        //Random random1 = new Random();
+        //int randomInt = random1.nextInt(5);
 
         MagicBox<String> boxStr = new MagicBox<>(3);
         boxStr.showBox();
-        boxStr.putValue([0]"First");
-        boxStr.showBox();
+        //boxStr.putValue([0], "First");
+        boxStr.add("Fe");
+        System.out.println();
+        System.out.println();
+
+        MagicBox<Integer> boxInt = new MagicBox<>(5);
+        System.out.println();
+        boxInt.showBox();
+
 
 
 
