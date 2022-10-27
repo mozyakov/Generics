ef6d3df5e087553ac49a5059e0b5eae01d32d48a
mozyakov <s.mozyakov@gmail.com>
2022-10-27 13:25:29 +0700

    понял

--- src/MagicBox.java
+++ src/MagicBox.java
@@ -1,41 +1,40 @@
+import java.util.*;
 public class MagicBox<T> {
-    private T value;
-    private int count = 0; //это показатель что коробка пустая, с null T value не получался boolean add
-    private final int countMax = 1; //кол-во объектов которое можно положить в него
-    public MagicBox() {} //конструктор оставил только такой
-    /*public MagicBox(T value) {
-        this.value = value;
-        this.count = count = 1;
-    }*/
+    protected T[] item;
+    //private int count = 0; //это показатель что коробка пустая, с null T value не получался boolean add
 
-    /*public MagicBox() {
-        MagicBox[] savedArray  = new MagicBox[3] ;
+    public MagicBox(int maxCount) {
+        item = (T[]) new Object[maxCount];
+    }
+
+    /*public MagicBox(int maxCount) { //конструктор
+        items = MagicBox[] savedArray  = new MagicBox[3] ;
     }*/
 
     /*public MagicBox() {
         T[] arrayItems = (T[]) new Object[3];
     }*/
 
-    public void putValue(T value) { //параметр метода типа T
+    /*public void putItem(T item) { //параметр метода типа T
         this.value = value;
-    }
+    }*/
 
-    public boolean add(T value) {
-        if (count == 0) {  //значит бокс пустой
-            this.value = value;
+    /*public boolean add(T items) {
+        if (T[] items == null) {  //значит бокс пустой
+            this.item = item;
             count += 1;  //для запрета дальнейших попыток положить в бокс
             return true;
         } else {
             return false;
         }
-    }
+    }*/
 
-    public T getValue() { // возвращает объект типа T
-        return value;
+    public T[] getItem() { // возвращает объект типа T
+        return item;
     }
     @Override
     public String toString() {
-        return "Содержимое коробки = "  + value;
+        return "Содержимое коробки = "  + item;
     }
     //public Account[] accounts = new Account[3];
 }
--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,26 @@ public class Main {
     public static void main(String[] args) {
         Random random1 = new Random();
         int randomInt = random1.nextInt(5);
+        String str1 = "First";
+        String str2 = "Second";
+        String str3 = "First";
 
-        MagicBox<String> boxStr1 = new MagicBox<>();
-        System.out.println(boxStr1 + " :null потому что бокс пустой");
-        System.out.println(boxStr1.add("First") + " :в бокс успешно добавлена строка First");
-        //System.out.println("проверка содержимого, д.б. First " + boxStr1);
-        System.out.println(boxStr1.add("try add") + " :false потому что бокс не пустой");
-        System.out.println(boxStr1);
-        System.out.println("демонстрация boxStr1 завершена ====================");
+        MagicBox<String> boxStr = new MagicBox<>(3);
+        System.out.println(boxStr + " :null потому что бокс пустой");
+        System.out.println(boxStr.add("First") + " :в бокс успешно добавлена строка First");
+        //System.out.println("проверка содержимого, д.б. First " + boxStr);
+        System.out.println(boxStr.add("try add") + " :false потому что бокс не пустой");
+        System.out.println(boxStr);
+        System.out.println("демонстрация boxStr завершена ====================");
         System.out.println();
 
-
-        MagicBox<Integer> boxInt1 = new MagicBox<>();
-        System.out.println("проверка содержимого, д.б. null " + boxInt1);
-        System.out.println(boxInt1.add(100) + " :успешно положили в бокс цифру 100");
-        System.out.println(boxInt1);
-        System.out.println(boxInt1.add(200) + " :200 не положилось, потому что бокс уже содержал 100"); //изменения не будет
-        System.out.println(boxInt1 + " :осталось 100 как и должно быть"); //подтверждение что изменения нет
-        System.out.println("демонстрация boxInt1 завершена ====================");
-        System.out.println();
-
+        MagicBox<Integer> boxInt = new MagicBox<>(3);
+        System.out.println("проверка содержимого, д.б. null " + boxInt);
+        System.out.println(boxInt.add(100) + " :положили в бокс цифру 100");
+        System.out.println(boxInt);
+        System.out.println(boxInt.add(200) + " :200 не положилось, потому что бокс уже содержит 100"); //изменения не будет
+        System.out.println(boxInt + " :осталось 100 как и должно быть"); //подтверждение что изменения нет
+        System.out.println("демонстрация boxInt завершена ====================");
 
     }
 }
